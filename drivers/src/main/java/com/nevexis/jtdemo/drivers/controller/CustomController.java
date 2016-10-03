80e996f868b6d775f89344b8cd2bf4ec159f6b11
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2016-10-03 14:52:58 +0300

    added CustomController that reads a message from application.properties

---
+++ drivers/src/main/java/com/nevexis/jtdemo/drivers/controller/CustomController.java
@@ -0,0 +1,24 @@
+package com.nevexis.jtdemo.drivers.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by evgeni on 10/3/2016.
+ */
+@RestController
+@RequestMapping("/custom")
+public class CustomController {
+    private String messageFromConfig;
+
+    public CustomController(@Value("${drivers.message}") String messageFromConfig) {
+        this.messageFromConfig = messageFromConfig;
+    }
+
+    @GetMapping("/message")
+    public String mssage(){
+        return this.messageFromConfig;
+    }
+}
