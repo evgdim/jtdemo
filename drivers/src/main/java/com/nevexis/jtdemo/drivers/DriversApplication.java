66009a3e0a896b3e8bfe09f6792a3491024fdc8b
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2016-10-04 11:17:10 +0300

    enable the discovery server and client

--- drivers/src/main/java/com/nevexis/jtdemo/drivers/DriversApplication.java
+++ drivers/src/main/java/com/nevexis/jtdemo/drivers/DriversApplication.java
@@ -2,7 +2,9 @@ package com.nevexis.jtdemo.drivers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DriversApplication {
 
--- drivers/src/main/java/com/nevexis/jtdemo/drivers/controller/CustomController.java
+++ drivers/src/main/java/com/nevexis/jtdemo/drivers/controller/CustomController.java
@@ -1,8 +1,14 @@
 package com.nevexis.jtdemo.drivers.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/custom")
 public class CustomController {
     private String messageFromConfig;
-
-    public CustomController(@Value("${drivers.message}") String messageFromConfig) {
+    private DiscoveryClient discoveryClient;
+    
+    public CustomController(@Value("${drivers.message}") String messageFromConfig, @Autowired DiscoveryClient discoveryClient) {
         this.messageFromConfig = messageFromConfig;
+        this.discoveryClient = discoveryClient;
     }
 
     @GetMapping("/message")
     public String mssage(){
         return this.messageFromConfig;
+    }
+    
+    @GetMapping("/services/{appName}")
+    public List<ServiceInstance> services(@PathVariable("appName") String appName){ 
+    	return this.discoveryClient.getInstances(appName);
     }
 }
