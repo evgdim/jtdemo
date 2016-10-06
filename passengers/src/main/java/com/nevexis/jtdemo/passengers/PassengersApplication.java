90e4a8cc3dea33c7c696ec8910aa1a864b978b81
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2016-10-06 10:51:40 +0300

    added passengers empty project

---
+++ passengers/src/main/java/com/nevexis/jtdemo/passengers/PassengersApplication.java
@@ -0,0 +1,12 @@
+package com.nevexis.jtdemo.passengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengersApplication.class, args);
+	}
+}
